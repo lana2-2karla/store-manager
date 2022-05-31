7d2747a4fc7bd47a0b32c1153406b60fcdd6c6a8
Allana Karla <allanakarla1702@gmail.com>
2022-05-31 00:03:12 -0300

    :sparkles: feat: adicionando caminho para as rotas

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const app = express();
+const productsController = require('./controllers/productsController');
+const salesController = require('./controllers/salesController');
+
+app.use('/', productsController);
+app.use('/', salesController);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
