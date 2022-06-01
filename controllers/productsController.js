ca7cabb4408187c57a553997bc0481ba893da9be
Allana Karla <allanakarla1702@gmail.com>
2022-06-01 18:19:50 -0300

    :sparkles: feat: criando rota POST /products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const productsValidateMiddleware = require('../middlewares/productsMiddleware');
 
 const router = express.Router();
 const productsService = require('../services/productsService');
@@ -17,5 +18,12 @@ router.get('/products/:id', async (req, res) => {
         const [rows] = await productsService.getAllProducts(id);
         if (rows.length !== 0) return res.status(200).json(rows[0]);
         return res.status(404).json({ message: 'Product not found' });
+});
+
+router.post('/products', productsValidateMiddleware, async (req, res) => {
+    const { name, quantity } = req.body;
+    const rows = await productsService.getNewProduct(name, quantity);
+    if (rows) return res.status(201).json(rows);
+    return res.status(409).json({ message: 'Product already exists' });
 });
  module.exports = router;
