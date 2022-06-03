5dbcbb1ec26f06e5680f419de6bc6ce74e497cdd
Allana Karla <allanakarla1702@gmail.com>
2022-06-03 17:02:49 -0300

    :sparkles: feat: criando rota /sales método POST

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -5,7 +5,7 @@ const salesService = require('../services/salesService');
 
 router.get('/sales', async (_req, res) => {
     try {
-        const [rows] = await salesService.getAllSales();
+        const [rows] = await salesService.getAllSalesService();
         return res.status(200).json(rows);
     } catch (err) {
         return res.status(404).json({ message: 'Sale not found' });
@@ -14,9 +14,14 @@ router.get('/sales', async (_req, res) => {
 
 router.get('/sales/:id', async (req, res) => {
         const { id } = req.params;
-        const [rows] = await salesService.getAllSales(id);
+        const [rows] = await salesService.getAllSalesService(id);
         if (rows.length === 0) return res.status(404).json({ message: 'Sale not found' });
        return res.status(200).json(rows);
+});
+
+router.post('/sales', async (req, res) => {
+  const infoSalesProducts = await salesService.getNewSalesService(req.body);
+  return res.status(201).json(infoSalesProducts);
 });
 
 module.exports = router;
