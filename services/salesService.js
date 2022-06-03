63ae1de11ecd6fcf28c72ed76e7448890803ce34
Allana Karla <allanakarla1702@gmail.com>
2022-06-03 17:02:03 -0300

    :sparkles: feat: criando função de verificação ao adicionar as vendas

--- services/salesService.js
+++ services/salesService.js
@@ -1,12 +1,24 @@
 const salesModel = require('../models/salesModel');
 
-const getAllSales = (id = null) => {
+const getAllSalesService = (id = null) => {
     if (id) {
         return salesModel.getByIdSales(id);
     }
     return salesModel.getAllSales();
 };
 
+const getNewSalesService = async (sales) => {
+    const salesId = await salesModel.addSales();
+    const newSales = await Promise
+    .all(sales.map(({ productId, quantity }) => salesModel
+    .addSalesProducts(salesId, productId, quantity)));
+    return {
+        id: salesId,
+        itemsSold: newSales,
+    };
+ };
+
 module.exports = {
-    getAllSales,
+    getAllSalesService,
+    getNewSalesService,
 };
