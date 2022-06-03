ff70a4c322b53303b35157a6ed86f799f78b0389
Allana Karla <allanakarla1702@gmail.com>
2022-06-03 18:01:08 -0300

    :sparkles: feat: criando rota /sales/id método PUT

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -13,15 +13,21 @@ router.get('/sales', async (_req, res) => {
 });
 
 router.get('/sales/:id', async (req, res) => {
-        const { id } = req.params;
-        const [rows] = await salesService.getAllSalesService(id);
-        if (rows.length === 0) return res.status(404).json({ message: 'Sale not found' });
-       return res.status(200).json(rows);
+    const { id } = req.params;
+    const [rows] = await salesService.getAllSalesService(id);
+    if (rows.length === 0) return res.status(404).json({ message: 'Sale not found' });
+    return res.status(200).json(rows);
 });
 
 router.post('/sales', async (req, res) => {
   const infoSalesProducts = await salesService.getNewSalesService(req.body);
   return res.status(201).json(infoSalesProducts);
+});
+
+router.put('/sales/:id', async (req, res) => {
+    const { id } = req.params;
+    const updateSalesProducts = await salesService.updateSalesService(id, req.body);
+    return res.status(200).json(updateSalesProducts);
 });
 
 module.exports = router;
