8b8110f192a504d5d7410c1ce7f0e705ce8410bb
Allana Karla <allanakarla1702@gmail.com>
2022-06-09 18:50:46 -0300

    :recycle: refactor: retirando as rotas realocadas

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,46 +1,45 @@
-const express = require('express');
-const productsValidateMiddleware = require('../middlewares/productsMiddleware');
-
-const router = express.Router();
 const productsService = require('../services/productsService');
-// const model = require('../models/productsModel');
 
-router.get('/products', async (_req, res) => {
-    try {
+const getAllProductsController = async (_req, res) => {
         const [rows] = await productsService.getAllProducts();
+        console.log(res);
         return res.status(200).json(rows);
-    } catch (err) {
-        return res.status(404).json({ message: 'Product not found' });
-    }
-});
+};
 
-router.get('/products/:id', async (req, res) => {
-        const { id } = req.params;
-        const [rows] = await productsService.getAllProducts(id);
-        if (rows.length !== 0) return res.status(200).json(rows[0]);
-        return res.status(404).json({ message: 'Product not found' });
-});
+const getByIdController = async (req, res) => {
+    const { id } = req.params;
+    const [rows] = await productsService.getAllProducts(id);
+    if (rows.length !== 0) return res.status(200).json(rows[0]);
+    return res.status(404).json({ message: 'Product not found' });
+};
 
-router.post('/products', productsValidateMiddleware, async (req, res) => {
+ const addProductsController = async (req, res) => {
     const { name, quantity } = req.body;
     const rows = await productsService.getNewProduct(name, quantity);
     if (rows) return res.status(201).json(rows);
     return res.status(409).json({ message: 'Product already exists' });
-});
+};
 
-router.put('/products/:id', async (req, res) => {
+const updateProductsController = async (req, res) => {
     const { id } = req.params;
     const { name, quantity } = req.body;
     const rows = await productsService.updateProductService(name, quantity, id);
     const [product] = await productsService.getAllProducts(id);
     if (rows === 1) return res.status(200).json(product[0]);
     return res.status(404).json({ message: 'Product not found' });
-});
+};
 
-router.delete('/products/:id', async (req, res) => {
+const deleteProductsController = async (req, res) => {
     const { id } = req.params;
     const isDeleted = await productsService.deleteProductService(id);
     if (isDeleted) return res.status(204).end();
     return res.status(404).json({ message: 'Product not found' });
-});
- module.exports = router;
+};
+
+ module.exports = {
+    getAllProductsController,
+    getByIdController,
+    addProductsController,
+    updateProductsController,
+    deleteProductsController,
+ };
--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,40 +1,43 @@
-const express = require('express');
-
-const router = express.Router();
 const salesService = require('../services/salesService');
 
-router.get('/sales', async (_req, res) => {
+const getAllSalesController = async (_req, res) => {
     try {
         const [rows] = await salesService.getAllSalesService();
         return res.status(200).json(rows);
     } catch (err) {
         return res.status(404).json({ message: 'Sale not found' });
     }
-});
+};
 
-router.get('/sales/:id', async (req, res) => {
+const getByIdSalesController = async (req, res) => {
     const { id } = req.params;
     const [rows] = await salesService.getAllSalesService(id);
     if (rows.length === 0) return res.status(404).json({ message: 'Sale not found' });
     return res.status(200).json(rows);
-});
+};
 
-router.post('/sales', async (req, res) => {
+const addSalesController = async (req, res) => {
   const infoSalesProducts = await salesService.getNewSalesService(req.body);
   return res.status(201).json(infoSalesProducts);
-});
+};
 
-router.put('/sales/:id', async (req, res) => {
+const updateSalesController = async (req, res) => {
     const { id } = req.params;
     const updateSalesProducts = await salesService.updateSalesService(id, req.body);
     return res.status(200).json(updateSalesProducts);
-});
+};
 
-router.delete('/sales/:id', async (req, res) => {
+const deleteSalesController = async (req, res) => {
     const { id } = req.params;
     const isDeleted = await salesService.deleteSalesService(id);
     if (isDeleted) return res.status(204).end();
     return res.status(404).json({ message: 'Sale not found' });
-});
+};
 
-module.exports = router;
+module.exports = {
+    getAllSalesController,
+    getByIdSalesController,
+    addSalesController,
+    updateSalesController,
+    deleteSalesController,
+};
